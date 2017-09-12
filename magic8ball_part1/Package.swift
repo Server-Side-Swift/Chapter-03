d4bd8c9ce3d9c9cc18f63a8da2fa551e2c6495d8
Jonathan Guthrie <jono@perfect.org>
2017-09-12 10:10:53 -0400

    adding part 1

---
+++ magic8ball_part1/Package.swift
@@ -0,0 +1,10 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-07-31 19:43:50 +0000
+import PackageDescription
+let package = Package(
+	name: "magic8ball",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+	]
+)
---
+++ magic8ball_part1/Sources/main.swift
@@ -0,0 +1,119 @@
+//
+//  main.swift
+//  PerfectTemplate
+//
+//  Created by Kyle Jessup on 2015-11-05.
+//	Copyright (C) 2015 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import Foundation
+
+// An example request handler.
+// This 'handler' function can be referenced directly in the configuration below.
+func handler(data: [String:Any]) throws -> RequestHandler {
+	return {
+		request, response in
+
+		response.setHeader(.contentType, value: "text/html")
+
+		let question = request.param(name: "q") ?? ""
+		if request.method == .post, !question.isEmpty {
+			var answer = "<p>\(question)</p>"
+			answer += "<h3>\(the8ball())</h3>"
+			response.setBody(string: answer)
+		}
+
+		var html = "<form method=\"POST\">"
+		html += "What question would you like to ask the Magic 8-Ball?"
+		html += "<br>"
+		html += "<input type=\"text\" name=\"q\">"
+		html += "<input type=\"submit\" value=\"Ask!\">"
+		html += "</form>"
+		response.appendBody(string: html)
+
+		response.completed()
+	}
+}
+
+func the8ball() -> String {
+	var answers = [String]()
+	answers.append("It is certain")
+	answers.append("It is decidedly so")
+	answers.append("Without a doubt")
+	answers.append("Yes definitely")
+	answers.append("You may rely on it")
+	answers.append("As I see it, yes")
+	answers.append("Most likely")
+	answers.append("Outlook good")
+	answers.append("Yes")
+	answers.append("Signs point to yes")
+	answers.append("Reply hazy try again")
+	answers.append("Ask again later")
+	answers.append("Better not tell you now")
+	answers.append("Cannot predict now")
+	answers.append("Concentrate and ask again")
+	answers.append("Don't count on it")
+	answers.append("My reply is no")
+	answers.append("My sources say no")
+	answers.append("Outlook not so good")
+	answers.append("Very doubtful")
+
+	return answers[Int(arc4random() % 20)]
+}
+
+
+// Configuration data for an example server.
+// This example configuration shows how to launch a server
+// using a configuration dictionary.
+
+
+let confData = [
+	"servers": [
+		// Configuration data for one server which:
+		//	* Serves the hello world message at <host>:<port>/
+		//	* Serves static files out of the "./webroot"
+		//		directory (which must be located in the current working directory).
+		//	* Performs content compression on outgoing data when appropriate.
+		[
+			"name":"localhost",
+			"port":8181,
+			"routes":[
+				["method":"get", "uri":"/", "handler":handler],
+				["method":"post", "uri":"/", "handler":handler],
+				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
+				 "documentRoot":"./webroot",
+				 "allowResponseFilters":true]
+			],
+			"filters":[
+				[
+				"type":"response",
+				"priority":"high",
+				"name":PerfectHTTPServer.HTTPFilter.contentCompression,
+				]
+			]
+		]
+	]
+]
+
+do {
+	// Launch the servers based on the configuration data.
+	try HTTPServer.launch(configurationData: confData)
+} catch {
+	fatalError("\(error)") // fatal error launching one of the servers
+}
+
+
